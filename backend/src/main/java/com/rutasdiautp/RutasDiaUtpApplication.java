7a4b778b99212f1022f60a77362323129ab171b0
Renzo Coveñas <renzojcr@hotmail.com>
2026-09-15 20:50:00 -0500

    feat(backend): bootstrap Spring Boot application

---
+++ backend/src/main/java/com/rutasdiautp/RutasDiaUtpApplication.java
@@ -0,0 +1,13 @@
+package com.rutasdiautp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RutasDiaUtpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RutasDiaUtpApplication.class, args);
+	}
+
+}
